36d10318d787ba3cda19b25327106d32c9b12e7a
tzmanics <tzmanics@gmail.com>
2017-03-20 00:39:23 -0400

    add array chart data & properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = '🎶Fill Up My Chart Put Some Data In It🎶';
+  private arraySeries: any[] = [{
+    name: 'puppy',
+    data: [47, 53, 59, 54, 67, 48]
+    },{
+    name: 'cookie',
+    data: [48, 54, 100, 45, 57, 48]
+    },{
+    name: 'taco',
+    data: [76, 56, 59, 70, 66, 63]
+    },{
+    name: 'monkey',
+    data: [60, 61, 65, 62, 63, 61]
+    },{
+    name: 'kitten',
+    data: [17, 17, 17, 15, 17, 14]
+  }];
+  private arrayCategories: string[] = [
+    'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'
+  ];
 }
