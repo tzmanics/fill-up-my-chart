7d7c820397cfefcc74a60c53572a9785088b59ca
tzmanics <tzmanics@gmail.com>
2017-03-19 23:48:52 -0400

    install & import charts module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ChartsModule,
     HttpModule
   ],
   providers: [],
